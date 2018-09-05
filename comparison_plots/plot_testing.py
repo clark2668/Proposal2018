06f615f08c7ee08b9e083081e68920a55f5d6357
Brian Clark <clark.2668@osu.edu>
2018-09-04 23:51:28 -0400

    Add basic testing script for new class

---
+++ comparison_plots/plot_testing.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+import numpy as np #import numpy
+import matplotlib.pyplot as plt #import matplotlib
+from matplotlib.pyplot import rcParams
+rcParams['mathtext.default'] = 'regular'
+import math
+from scipy.interpolate import interp1d, splrep, splev
+from pylab import setp
+import detector_data as detector
+
+
+def main():
+
+
+
+	logenergy, limit = detector.get_limit('ara2_2016')
+	energy = np.power(10.,logenergy)
+
+	fig = plt.figure(figsize=(11,8.5))
+	ax_limit = fig.add_subplot(1,1,1)
+	ax_limit.plot(energy,limit,'-s', linewidth=2.0,color='blue',label=r'ARA2 Limit')
+	beautify_limit(ax_limit, 0)
+	fig.savefig("test_functions.png",edgecolor='none',bbox_inches="tight") #save the figure
+
+def beautify_aeff(this_ax):
+	sizer=20
+	xlow = 1.e15 #the lower x limit
+	xup = 1.e21 #the uppper x limit
+	ylow = 1.e3 #the lower x limit
+	yup = 1.e10 #the uppper x limit
+	this_ax.set_xlabel('Energy  [eV]',size=sizer) #give it a title
+	this_ax.set_ylabel('[A$\Omega]_{eff}$  [cm$^2$sr]',size=sizer)
+	this_ax.set_yscale('log')
+	this_ax.set_xscale('log')
+	this_ax.tick_params(labelsize=sizer)
+	this_ax.set_xlim([xlow,xup]) #set the x limits of the plot
+	this_ax.set_ylim([ylow,yup]) #set the y limits of the plot
+	this_ax.grid()
+	this_legend = this_ax.legend(loc='lower right',title='Analysis Level')
+	setp(this_legend.get_texts(), fontsize=17)
+	setp(this_legend.get_title(), fontsize=17)
+	#this_ax.set_yticks([1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1e0,1e1])
+
+
+
+#pass it the axes and the number of theory curves you're including
+#always pass the theory curves first
+def beautify_limit(this_ax, num_theory):
+	sizer=20
+	xlow =  1.e15 #the lower x limit
+	xup = 1e21 #the uppper x limit
+	ylow = 1e-20 #the lower y limit
+	yup = 1e-10 #the upper y limit
+	this_ax.set_xlabel('Energy [eV]',size=sizer) #give it a title
+	this_ax.set_ylabel('E F(E) [$cm^{-2} s^{-1} sr^{-1}$]',size=sizer)
+	this_ax.set_yscale('log')
+	this_ax.set_xscale('log')
+	this_ax.tick_params(labelsize=sizer)
+	this_ax.set_xlim([xlow,xup]) #set the x limits of the plot
+	this_ax.set_ylim([ylow,yup]) #set the y limits of the plot
+	handles, labels = this_ax.get_legend_handles_labels()
+	legend1 = this_ax.legend(handles[0:num_theory], labels[0:num_theory], loc='lower left',title='Flux Models')
+	this_ax.add_artist(legend1)
+	legend2 = this_ax.legend(handles[num_theory:], labels[num_theory:], loc='upper right',title='Analysis Level')
+	setp(legend1.get_texts(), fontsize=17)
+	setp(legend1.get_title(), fontsize=17)
+	setp(legend2.get_texts(), fontsize=17)
+	setp(legend2.get_title(), fontsize=17)
+	this_ax.grid()
+
+#pass it the axes and the number of theory curves you're including
+#always pass the theory curves first
+def beautify_counts(this_ax):
+	sizer=20
+	xlow =  1.e15 #the lower x limit
+	xup = 1e21 #the uppper x limit
+	ylow = 1e-20 #the lower y limit
+	yup = 1e-10 #the upper y limit
+	this_ax.set_xlabel('Energy [eV]',size=sizer) #give it a title
+	this_ax.set_ylabel('Events',size=sizer)
+	#this_ax.set_yscale('log')
+	this_ax.set_xscale('log')
+	this_ax.tick_params(labelsize=sizer)
+	this_ax.set_xlim([xlow,xup]) #set the x limits of the plot
+	#this_ax.set_ylim([ylow,yup]) #set the y limits of the plot
+	this_ax.grid()
+	this_legend = this_ax.legend(loc='lower right',title='Analysis Level Event Counts')
+	setp(this_legend.get_texts(), fontsize=17)
+	setp(this_legend.get_title(), fontsize=17)
+
+main()
